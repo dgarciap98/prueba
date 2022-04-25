a63ea62c8d456eec64621f907acb64dcc4df5f13
LaroPozuelo <lpozuelot@ceinmark.net>
2022-04-25 09:24:12 +0200

    laro

--- src/test/HolaMundo.java
+++ src/test/HolaMundo.java
@@ -5,6 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola, soy Paola");
+		System.out.println("Hola soy Laro");
 
 	}
 
