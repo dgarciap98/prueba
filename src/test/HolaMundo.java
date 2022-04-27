f568f514737dcd0f3206dcca180c64bb9205cbbb
Mateo <Mateo@DESKTOP-SEL1953>
2022-04-27 12:03:57 +0200

    mateo

--- src/test/HolaMundo.java
+++ src/test/HolaMundo.java
@@ -6,13 +6,10 @@ public class HolaMundo {
 		System.out.println("Hola Mundo");
 		System.out.println("Hola, soy Paola");
 		System.out.println("Hola soy Laro");
-<<<<<<< HEAD
-		System.out.println("Hola soy Mateo");
-=======
 		System.out.println("Hola, soy Pablo");
 		System.out.println("Hola, soy Miguel :)" );		
+		System.out.println("Hola soy Mateo");
 
->>>>>>> branch 'master' of https://github.com/dgarciap98/prueba.git
 	}
 
 }
