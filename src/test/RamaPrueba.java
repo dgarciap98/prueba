946b8a1a524bcadc2f691b5aade9ac92fd000e43
davis <davis@Portatil_David>
2022-04-27 11:57:17 +0200

    Primer commit Rama de Prueba

--- src/test/RamaPrueba.java
+++ src/test/RamaPrueba.java
@@ -1,11 +1,11 @@
 package test;
 
-public class HolaMundo {
+public class RamaPrueba {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		
+		System.out.println("Prueba de rama");
 
 	}
 
 }
- 
